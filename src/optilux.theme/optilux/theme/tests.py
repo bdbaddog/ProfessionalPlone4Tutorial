4e927c6162db4e73cc5912dbbeabe819db31a3d7
William Deegan <bill@baddogconsulting.com>
2011-10-13 13:04:33 -0700

    ch8. p 165. Tests now work

--- src/optilux.theme/optilux/theme/tests.py
+++ src/optilux.theme/optilux/theme/tests.py
@@ -1,55 +1,65 @@
-import unittest
+import unittest2 as unittest
+import transaction
 
-#from zope.testing import doctestunit
-#from zope.component import testing
-from Testing import ZopeTestCase as ztc
+from optilux.theme.testing import OPTILUX_THEME_INTEGRATION_TESTING
+from optilux.theme.testing import OPTILUX_THEME_FUNCTIONAL_TESTING
 
-from Products.Five import fiveconfigure
-from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite
-ptc.setupPloneSite()
+from plone.testing.z2 import Browser
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
 
-import optilux.theme
+from zope.component import getUtility
+from Products.CMFCore.utils import getToolByName
 
+from plone.registry.interfaces import IRegistry
+from plone.app.theming.interfaces import IThemeSettings
 
-class TestCase(ptc.PloneTestCase):
+class TestSetup(unittest.TestCase):
+    layer= OPTILUX_THEME_INTEGRATION_TESTING
+    def test_css_registry_configured(self):
+        portal = self.layer['portal']
+        cssRegistry = getToolByName(portal,'portal_css')
+        self.assertTrue(
+            "++theme++optilux.theme/stylesheets/main.css"
+            in cssRegistry.getResourceIds()
+            )
+        self.assertTrue(
+            "++theme++optilux.theme/stylesheets/iefixes.css"
+            in cssRegistry.getResourceIds()
+            )
 
-    class layer(PloneSite):
+    def test_theme_configured(self):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IThemeSettings)
+        self.assertEqual(settings.enabled,True)
+        self.assertEqual(settings.rules,
+                         '/++theme++optilux.theme/rules.xml')
+        self.assertEqual(settings.absolutePrefix,
+                         "/++theme++optilux.theme")
 
-        @classmethod
-        def setUp(cls):
-            fiveconfigure.debug_mode = True
-            ztc.installPackage(optilux.theme)
-            fiveconfigure.debug_mode = False
+class TestRendering(unittest.TestCase):
+    layer = OPTILUX_THEME_FUNCTIONAL_TESTING
 
-        @classmethod
-        def tearDown(cls):
-            pass
+    def test_render_plone_page(self):
+        app = self.layer['app']
+        portal = self.layer['portal']
+        transaction.commit()
 
+        browser=Browser(app)
+        browser.open(portal.absolute_url())
+        self.assertTrue('<div id="wrapper">' in browser.contents)
 
-def test_suite():
-    return unittest.TestSuite([
+    def test_render_zmi_page(self):
+        app = self.layer['app']
+        portal = self.layer['portal']
+        transaction.commit()
 
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='optilux.theme',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
+        browser=Browser(app)
+        browser.addHeader('Authorization','Basic %s:%s'%(SITE_OWNER_NAME,SITE_OWNER_PASSWORD,))
+        browser.open(portal.absolute_url() + '/manage_main')
+        self.assertFalse('<div id="wrapper">' in browser.contents)
 
-        #doctestunit.DocTestSuite(
-        #    module='optilux.theme.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='optilux.theme',
-        #    test_class=TestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='optilux.theme',
-        #    test_class=TestCase),
-
-        ])
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
+        
+        
+        
+        
